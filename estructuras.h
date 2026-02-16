e9306620ac10fade14b11a937c1eca4e2e34670c
FabianFerReyes <fernandafabianreyes@gmail.com>
2026-02-16 10:32:59 -0600

    se creó el tipo de emumeracion para los tipos de instrucciones

--- estructuras.h
+++ estructuras.h
@@ -1,5 +1,63 @@
 #ifndef estructuras
 #define estructuras 
 
+typedef struct Token{
+    char *textoToken;
+    struct Token *sig;
+} Token;
+
+typedef struct Renglon{
+    char *texto;
+    struct Renglon *sig;
+    Token *primerToken;
+    Token *ultimoToken;
+} Renglon;
+
+typedef struct {
+    Renglon *inicio;
+    Renglon *final;
+    int tamanio;
+} Archivo;
+
+/*------------ TIPO DE INSTRUCCION  ---------------------------------*/ 
+enum tipoInstruccion {
+    MOV = 1,    ADD,
+    MUL,        DIV,
+    INC,        DEC,
+    END
+}
+
+/*------------ ERRORES Y ADVERTENCIAS ---------------------------------*/ 
+// todo bien = 0
+enum errorArchivo {
+    /*-- Errores --*/
+    NOMBRE_INCORRECTO = 100,
+    NO_HAY_ARCHIVO,
+    NO_HAY_TEXTO
+};
+
+enum errorLexico {
+    /*-- Errores --*/
+    INICIA_EN_NUMERO = 300,
+    TIENE_MINUSCULA,
+    CARACTERES_ESPECIALES
+};
+
+enum errorSintactico {
+    /*-- Errores --*/
+    PARAMETROS_INSUFICIENTES = 400,
+    PARAMETROS_EXTRA,
+    PARAMETROS_INCORRECTOS,
+
+    /*-- Advertencias --*/
+    ESPACIOS_EXTRA = 450,
+    SALTOS_LINEA_EXTRA
+};
+
+enum errorToken {
+    /*-- Errores --*/
+    NO_HAY_TOKENS = 200,
+};
+
 
 #endif
--- prototipos.h
+++ prototipos.h
@@ -1,57 +1,6 @@
 #ifndef prototipos
 #define prototipos 
 
-typedef struct Token{
-    char *textoToken;
-    struct Token *sig;
-} Token;
-
-typedef struct Renglon{
-    char *texto;
-    struct Renglon *sig;
-    Token *primerToken;
-    Token *ultimoToken;
-} Renglon;
-
-typedef struct {
-    Renglon *inicio;
-    Renglon *final;
-    int tamanio;
-} Archivo;
-
-// todo bien = 0
-/*------------ ERRORES Y ADVERTENCIAS ---------------------------------*/ 
-enum errorArchivo {
-    /*-- Errores --*/
-    NOMBRE_INCORRECTO = 100,
-    NO_HAY_ARCHIVO,
-    NO_HAY_TEXTO
-};
-
-enum errorLexico {
-    /*-- Errores --*/
-    INICIA_EN_NUMERO = 300,
-    TIENE_MINUSCULA,
-    CARACTERES_ESPECIALES
-};
-
-enum errorSintactico {
-    /*-- Errores --*/
-    PARAMETROS_INSUFICIENTES = 400,
-    PARAMETROS_EXTRA,
-    PARAMETROS_INCORRECTOS,
-
-    /*-- Advertencias --*/
-    ESPACIOS_EXTRA = 450,
-    SALTOS_LINEA_EXTRA
-};
-
-enum errorToken {
-    /*-- Errores --*/
-    NO_HAY_TOKENS = 200,
-};
-
-
 /*---------- ARCHIVOS --------------------------------*/
 Archivo* crearArchivo();
 Renglon* crearRenglon(char *texto);
