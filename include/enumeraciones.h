8c8f1f3368f7f812548f9dd2c5814cbf801e8633
FerFabianReyes <2100721a@umich.mx>
2026-03-09 10:41:10 -0600

    Se separaron las estructuras de las enumeraciones

---
+++ include/enumeraciones.h
@@ -0,0 +1,53 @@
+#ifndef enumaraciones
+#define enumeraciones
+
+/*------------ TIPO DE PARAMETRO  ---------------------------------*/ 
+typedef enum tipoParametro{
+    INSTR = 1, REG_COM, REG, NUM
+}tipoParametro;
+
+/*------------ COMANDO EN LA TERMINAL  ---------------------------------*/ 
+typedef enum comandoTerminal{
+    SALIR = 10, EJECUTAR_ARCHIVO
+}comandoTerminal;
+
+/*------------ ESTADOS DEL SISTEMA  ---------------------------------*/ 
+typedef enum estadoSistema{
+    EJECUCION = 50, ESPERA,
+}estadoSistema;
+
+/*------------ ERRORES Y ADVERTENCIAS ---------------------------------*/ 
+typedef enum errorAdvertencia{
+    BIEN = 0,
+/*----- Archivos -----*/ 
+    // Errores
+    NOMBRE_INCORRECTO = 100,
+    NO_HAY_ARCHIVO,
+    NO_HAY_TEXTO,
+
+/*----- Tokens -----*/ 
+    //Errores
+    NO_HAY_TOKENS = 200,
+
+/*----- Lexico -----*/ 
+    // Errores
+    INICIA_EN_NUMERO = 300,
+    TIPO_PARAM_INVALIDO,
+    INICIA_MINUSCULA,
+    REGISTRO_INVALIDO,
+    NUM_DECIMAL,
+
+/*----- Sintáctico -----*/ 
+     // Erroes
+    INSTR_REG_INCUMPLIDA = 400, 
+    PARAMETROS_INCORRECTOS,
+    PARAMETROS_INSUFICIENTES,
+    PARAMETROS_EXTRA,
+    DIV_ENTRE_CERO,
+    ESPACIOS_EXTRA,
+
+/*----- Comandos -----*/ 
+    COMANDO_INVALIDO = 500
+}errorAdvertencia;
+
+#endif
